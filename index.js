f6af26ccf7daa4849c860f681c1dc9c99f42afe1
Jeffrey Marks <JKDMarks@gmail.com>
2019-05-28 16:46:33 -0400

    Revise MVO

--- index.js
+++ index.js
@@ -3,165 +3,109 @@ const USERS_URL = "https://swimmy-otter-backend.herokuapp.com/api/v1/users"
 const grab = (selectorStr, parent = document) => parent.querySelector(selectorStr)
 
 document.addEventListener('DOMContentLoaded', event => {
+  const img = document.createElement("img")
+  img.src = "./otter-sprite.png"
+
   const stage = new createjs.Stage("canvas")
 
   const circle = new createjs.Shape()
-  circle.graphics.beginFill("red").drawCircle(0, 0, 10)
-  circle.radius = 5
+  circle.graphics.beginFill("red").drawCircle(0, 0, 25)
+  circle.radius = 25
   circle.diameter = 2 * circle.radius
 
   circle.x = canvas.width/2
-  circle.y = canvas.height - 10
+  circle.y = canvas.height - circle.radius
 
-  const leftBound = circle.diameter
-  const rightBound = canvas.width - circle.diameter
-  const topBound = circle.diameter
-  const bottBound = canvas.height - circle.diameter
+  const leftBound = circle.radius
+  const rightBound = canvas.width - circle.radius
 
   stage.addChild(circle)
   stage.update()
 
-  let moveY = ""
   let moveX = ""
-  let gravitySpeed = 0
-  let gravity = 0.02
   let canMove = true
 
   document.addEventListener('keydown', e => {
-    if (canMove) {
-      if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-        moveY = e.key
-      }
-
-      if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
-        moveX = e.key
-      }
-    }
-
-    if (e.key === "Enter") {
-      canMove = !canMove
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+      moveX = e.key
     }
   })
 
   document.addEventListener('keyup', (e) => {
-    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-      moveY = ""
-    }
-
     if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
       moveX = ""
     }
   })
 
-  createjs.Ticker.framerate = 80
-  createjs.Ticker.addEventListener("tick", moveCirc)
+  createjs.Ticker.framerate = 60
+  createjs.Ticker.addEventListener("tick", gameMovement)
 
-  function moveCirc() {
-    if (circle.y < bottBound) {
-      gravitySpeed += gravity
-      circle.y += gravitySpeed
-      stage.update()
-    } else {
-      gravitySpeed = 0
-      canMove = true
+  let t = 0
+
+  function gameMovement() {
+    t += 1
+    if (t % 120 === 0) {
+      addPlat()
     }
 
-    if (gravitySpeed >= 3) {
-      canMove = false
-      moveX = ""
-      moveY = ""
-    }
-
-    if (moveY === "ArrowUp" && circle.y > topBound) {
-      circle.y -= 3
+    if (hasCollided()) {
+      stage.removeChild(...platArr)
       stage.update()
-    } else if (moveY === "ArrowDown" && circle.y < bottBound) {
-      circle.y += 3
-      stage.update()
+      platArr = []
+      alert(`You lose. Final time: ${Math.floor(t/60)} seconds`)
+      t = 0
     }
 
     if (moveX === "ArrowLeft" && circle.x > leftBound) {
-      circle.x -= 2
+      circle.x -= 3
       stage.update()
     } else if (moveX === "ArrowRight" && circle.x < rightBound) {
-      circle.x += 2
+      circle.x += 3
+      stage.update()
+    }
+
+    for (const plat of platArr) {
+      plat.y += 5
       stage.update()
     }
   }
 
-  const platArr = []
+  let platArr = []
 
-  for (let i=1; i<=7; i++) {
+  function addPlat() {
     const plat = new createjs.Shape()
-    plat.graphics.beginFill("blue").drawRect(0, 0, 50, 10)
+    plat.graphics.beginFill("blue").drawRect(0, 0, 10, 50)
+    plat.w = 10
+    plat.h = 50
     plat.x = Math.floor(Math.random() * 450)
-    plat.y = i*100
+    plat.y = 0
     stage.addChild(plat)
     stage.update()
     platArr.push(plat)
   }
 
-  function collisionY() {
-    // for (const plat of platArr) {
-    //
-    // }
+  function hasCollided() {
+    let collided
+    for (const plat of platArr) {
+      if (plat.x < circle.x + circle.radius &&
+        plat.x + plat.w > circle.x - circle.radius &&
+        plat.y < circle.y + circle.radius &&
+        plat.h + plat.y > circle.y - circle.radius
+      ) {
+        collided = true
+        break
+      } else {
+        collided = false
+      }
+    }
+    return collided
   }
 
-  function collisionX() {}
-
-  // let moveAmt = 10
-  //
-  // function handleTick() {
-  //   circle.x += moveAmt
-  //   if (circle.x > stage.canvas.width || circle.x < 0) { moveAmt *= -1}
-  //   stage.update()
-  // }
-
-  // fetch(USERS_URL, {method: 'GET'}).then(r => r.json()).then(json => {
-  //   console.log(json)
-  //   console.log(canvas)
-  // })
+  const pauseBtn = grab("#pause")
+  pauseBtn.addEventListener("click", () => {
+    console.log(circle)
+    console.log(platArr)
+    debugger
+  })
 
 })
-
-
-// function startGame() {
-//     myGameArea.start();
-// }
-//
-// var myGameArea = {
-//   this.interval = setInterval(updateGameArea, 20);
-// }
-//
-// function component(width, height, color, x, y, type) {
-//     this.gravity = 0.05;
-//     this.gravitySpeed = 0;
-//     this.update = function() {
-//         ctx = myGameArea.context;
-//         ctx.fillStyle = color;
-//         ctx.fillRect(this.x, this.y, this.width, this.height);
-//     }
-//     this.newPos = function() {
-//         this.gravitySpeed += this.gravity;
-//         this.x += this.speedX;
-//         this.y += this.speedY + this.gravitySpeed;
-//         this.hitBottom();
-//     }
-//     this.hitBottom = function() {
-//         var rockbottom = myGameArea.canvas.height - this.height;
-//         if (this.y > rockbottom) {
-//             this.y = rockbottom;
-//             this.gravitySpeed = 0;
-//         }
-//     }
-// }
-//
-// function updateGameArea() {
-//     myGameArea.clear();
-//     myGamePiece.newPos();
-//     myGamePiece.update();
-// }
-//
-// function accelerate(n) {
-//     myGamePiece.gravity = n;
-// }
