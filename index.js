ca4dba026377f579adff804124b7c7994335c952
Jeffrey Marks <JKDMarks@gmail.com>
2019-05-29 14:17:17 -0400

    Otter and logs. Increasing speed.

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const USERS_URL = "https://swimmy-otter-backend.herokuapp.com/api/v1/users"
 const grab = (selectorStr, parent = document) => parent.querySelector(selectorStr)
 
 document.addEventListener('DOMContentLoaded', event => {
-  const img = document.createElement("img")
-  img.src = "./otter-sprite.png"
 
   const stage = new createjs.Stage("canvas")
 
-  const circle = new createjs.Shape()
-  circle.graphics.beginFill("red").drawCircle(0, 0, 25)
-  circle.radius = 25
-  circle.diameter = 2 * circle.radius
+  const otterImg = new Image();
+  otterImg.src = "./images/otter-sprite.png";
+  otterImg.onload = handleOtterImageLoad;
+  let otter
+  let leftBound
+  let rightBound
 
-  circle.x = canvas.width/2
-  circle.y = canvas.height - circle.radius
+  function handleOtterImageLoad(event) {
+    const image = event.target;
+    otter = new createjs.Bitmap(image);
+    stage.addChild(otter);
+    otter.w = 50
+    otter.h = 64
+    otter.x = canvas.width/2 - otter.w/2
+    otter.y = canvas.height - otter.h
 
-  const leftBound = circle.radius
-  const rightBound = canvas.width - circle.radius
-
-  stage.addChild(circle)
-  stage.update()
+    leftBound = 0
+    rightBound = canvas.width - otter.w
+  }
 
   let moveX = ""
   let canMove = true
@@ -41,57 +45,68 @@ document.addEventListener('DOMContentLoaded', event => {
   createjs.Ticker.addEventListener("tick", gameMovement)
 
   let t = 0
+  let dropFreq = 60
 
   function gameMovement() {
     t += 1
-    if (t % 30 === 0) {
-      addPlat()
+
+    if (t % dropFreq === 0) {
+      addLog()
+      if (dropFreq > 10) dropFreq -= 1
     }
 
     if (hasCollided()) {
-      stage.removeChild(...platArr)
+      stage.removeChild(...logArr)
       stage.update()
-      platArr = []
+      logArr = []
       alert(`You lose. Final time: ${Math.floor(t/60)} seconds`)
+      dropFreq = 60
       t = 0
     }
 
-    if (moveX === "ArrowLeft" && circle.x > leftBound) {
-      circle.x -= 4
+    if (moveX === "ArrowLeft" && otter.x > leftBound) {
+      otter.x -= 4
       stage.update()
-    } else if (moveX === "ArrowRight" && circle.x < rightBound) {
-      circle.x += 4
+    } else if (moveX === "ArrowRight" && otter.x < rightBound) {
+      otter.x += 4
       stage.update()
     }
 
-    for (const plat of platArr) {
-      plat.y += plat.speed
+    for (let i=0; i < logArr.length; i++) {
+      logArr[i].y += logArr[i].speed
       stage.update()
+      if (logArr[i].y > canvas.height) {
+        stage.removeChild(logArr[i])
+        logArr.splice(i, 1)
+        i--
+      }
     }
   }
 
-  let platArr = []
+  let logArr = []
 
-  function addPlat() {
-    const plat = new createjs.Shape()
-    plat.graphics.beginFill("blue").drawRect(0, 0, 10, 50)
-    plat.w = 10
-    plat.h = 50
-    plat.x = Math.floor(Math.random() * 450)
-    plat.y = 0
-    plat.speed = Math.ceil(Math.random() * 10) + Math.floor(t/120) //random num 1~10, base +1 every 2s
-    stage.addChild(plat)
+  function addLog() {
+
+    const randomLogImg = logImgsArr[Math.floor(logImgsArr.length * Math.random())]
+    const log = new Log(randomLogImg).log
+
+    log.speed = Math.ceil(Math.random() * 10) + Math.floor(t/120) //random num 1~10, base +1 every 2s
+
+    console.log(log.speed, Math.floor(t/120), dropFreq)
+
+    stage.addChild(log)
     stage.update()
-    platArr.push(plat)
+    logArr.push(log)
   }
 
   function hasCollided() {
     let collided
-    for (const plat of platArr) {
-      if (plat.x < circle.x + circle.radius &&
-        plat.x + plat.w > circle.x - circle.radius &&
-        plat.y < circle.y + circle.radius &&
-        plat.h + plat.y > circle.y - circle.radius
+    for (const log of logArr) {
+      if (
+        log.x < otter.x + otter.w &&
+        log.x + log.w > otter.x &&
+        log.y < otter.y + otter.h &&
+        log.h + log.y > otter.y
       ) {
         collided = true
         break
@@ -104,8 +119,8 @@ document.addEventListener('DOMContentLoaded', event => {
 
   const pauseBtn = grab("#pause")
   pauseBtn.addEventListener("click", () => {
-    console.log(circle)
-    console.log(platArr)
+    console.log(logArr)
+    console.log(otter)
     debugger
   })
 
